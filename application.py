a3ee786140c2df1e8c50e00c9ec133be941db8af
Cado Fett <brandon@krabach.net>
2025-05-01 16:06:03 -0400

    Cleaned up
    application.py only used index.html, so I got rid of the TODO function. It now can have a custom port using the server's os, and can be set within the hosting website as an env PORT

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -8,9 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/TODO')
-def todo():
-    return render_template('TODO.html')
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+# Finish Application code
+# Persist in browser (javascript)
+# Persist in server (python)
